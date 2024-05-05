e349ffcffb1c2600b2a7c079b33b80262b56a1be
Pivo <horriblebuba@gmail.com>
2024-05-05 13:07:42 +0500

    setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='treenode-py',
-    version='1.2.0',
+    version='1.3.0',
     packages=find_packages(),
     license='MIT',
     description='treenode-py (treenode) is a Python library that provides functionality to create and manipulate tree structures.',
